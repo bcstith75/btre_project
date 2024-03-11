c1b33c5503f1fad9c1966127bdaa46c50dda9944
bcstith75 <bcstith75@yahoo.com>
2024-03-11 14:07:28 -0400

    added local settings code

--- btre/settings.py
+++ btre/settings.py
@@ -148,6 +148,11 @@ MESSAGE_TAGS = {
 # Email config 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 578
-EMAIL_HOST_USER = 'bcstith75@gmail.com'
-EMAIL_HOST_PASSWORD = 'player07'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
